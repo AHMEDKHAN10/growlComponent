3e04d9ee7e47f148bb63019f9ad0e729f32f0afc
Ahmed Khan <khanmuham@deakin.edu.au>
2023-02-01 18:53:01 +1100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import { Growl, useGrowl } from "./lib/growl";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App () {
+  const [active, setActive] = useGrowl()
+  const handleClick = () => {
+    setActive({state: true, timer: 3000})
+  }
+  return (
+    <div style={{ width: 640, margin: "15px auto" }}>
+      <h1>Hello React</h1>
+      <a className="App-link" href="#" onClick={handleClick}>
+					Clik here to activate the growl
+        		</a>
+      <Growl onDismissed={() => setActive({state: false, timer: 0})} active={active.state} message="Hello World!" />
+    </div>
+  )
+};
+ReactDOM.render(<App/>, document.getElementById("root"));
